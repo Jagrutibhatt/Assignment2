6fd78a29cee4cfc30a97fe43df784c8830d2577e
Jagrutibhatt <109428301+Jagrutibhatt@users.noreply.github.com>
2022-07-17 19:11:15 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,22 @@
+
+
+function add(x,y){
+    let a = "Addition is ";
+    return (a+(x+y));
+}
+function subtract(x,y){
+    const a  = "Subtraction is ";
+    return (a+(x-y));
+}
+function divide(x,y){
+    var a = "Dividation is ";
+    return (a+(x/y));
+}
+function multiply(x,y){
+    let a = "Multipication is ";
+    return (a + (x*y));
+}
+console.log("\n"+(add(20,30)+"\n"));
+console.log(subtract(20,30)+"\n");
+console.log(divide(20,30)+"\n");
+console.log(multiply(20,30)+"\n");
